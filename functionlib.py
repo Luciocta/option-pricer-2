3e52fc5be25e715890c22f6ada645bf88007bfa2
Lucio Cuttitta <cuttittalucio@icloud.com>
2025-03-06 16:11:15 +0100

    06/03

---
+++ functionlib.py
@@ -0,0 +1,100 @@
+import math
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+
+
+def black_scholes(S0, K, T, r, vol, q, opt_type="c"):
+    """
+    S0 : spot price at time t=0
+    K : strike price
+    T : maturity (in years)
+    r : risk-free rate
+    vol : implied volatility
+    opt_type : "C" for a call option and "P" for a put option
+
+    Return the price of a vanilla option (for either a call option or a put option) with Black-Scholes formula
+    """
+
+    d1=(np.log(S0/K) + (r-q+vol**2/2)*T)/(vol*np.sqrt(T))
+    d2=d1-vol*np.sqrt(T)
+    
+    try:
+        if opt_type=="c":
+            price=S0*np.exp(-q*T)*norm.cdf(d1, 0, 1)-K*np.exp(-r*T)*norm.cdf(d2, 0, 1)
+        elif opt_type=="c":
+            price=K*np.exp(-r*T)*norm.cdf(-d2, 0, 1)-S0*norm.cdf(-d1, 0, 1)
+        return round(price, 2)
+    except:
+        raise ValueError("Please confirm option type, either 'C' for Call or 'P' for Put")
+
+
+def monte_carlo(S0, T, r, vol, M, antithetic=False):
+    """
+    S0 : spot price at time t=0
+    T : maturity (in years)
+    r : risk-free rate
+    vol : implied volatility
+    M : number of simulations
+    antithetic : Boolean type (True to improve precision of results)
+
+    Return a matrix with M columns for each simulation where a simulation represents a path for the asset price
+    """
+
+    # Fix random seed for reproductibility
+    np.random.seed(123)
+
+    if T > 0:
+        N=int(round(T*252))   ## 252 times for each trading day in a year
+        dt=T/N
+    elif T == 0:
+        return pd.DataFrame(np.full((1, M), S0), columns=[f"{i+1}" for i in range(M)])
+    else:
+        raise ValueError("T should be positive")
+    
+    nudt=(r-0.5*vol**2)*dt
+    volsdt=vol*np.sqrt(dt)
+
+    if antithetic:
+        M=int(round(M/2))
+
+    epsilon=np.random.normal(0, 1, (N, M))
+    growth_factors=np.exp(nudt + volsdt * epsilon)
+    prices=S0*pd.DataFrame(growth_factors).cumprod(axis=0)
+
+    if antithetic:
+        growth_factors_ant=np.exp(nudt - volsdt * epsilon)
+        prices_ant=S0*pd.DataFrame(growth_factors_ant).cumprod(axis=0)
+        prices=pd.concat([prices, prices_ant], axis=1, ignore_index=True)
+
+    S=pd.concat([pd.DataFrame([S0] * prices.shape[1]).T, prices], ignore_index=True)
+    S.columns=[f"{i+1}" for i in range(prices.shape[1])]
+
+    return S
+
+
+def delta(S0, K, T, r, vol, q, opt_type='c'):
+    """
+    S0 : spot price at time t=0
+    K : strike price
+    T : maturity (in years)
+    r : risk-free rate
+    vol : implied volatility
+    opt_type : "C" for a call option and "P" for a put option
+
+    Return the delta of a vanilla option (for either a call option or a put option) with Black-Scholes formula
+    """
+    d1=(np.log(S0/K) + (r-q+vol**2/2)*T)/(vol*np.sqrt(T))
+
+    if opt_type=="c":
+        delta = norm.cdf(d1, 0, 1)
+    elif opt_type=="p":
+        delta = norm.cdf(d1, 0, 1) - 1
+    return round(delta, 2)
+
+def gamma(S0, K, T, r, vol):
+
+    d1=(np.log(S0/K) + (r+vol**2/2)*T)/(vol*np.sqrt(T))
+    Nprime = lambda x: np.exp(-x**2/2)/np.sqrt(2*np.pi)
+
+    return Nprime(d1)/(S0*vol*np.sqrt(T))
--- main.py
+++ main.py
@@ -3,21 +3,14 @@ import numpy as np
 from scipy.stats import norm
 import plotly.graph_objs as go
 import plotly.io as pio
+import functionlib as fl
 
 app = Flask(__name__)
-
-def black_scholes_call(S, K, T, r, sigma, q=0):
-    """
-    Calcule le prix d'un call européen selon la formule de Black-Scholes.
-    """
-    d1 = (np.log(S / K) + (r - q + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
-    d2 = d1 - sigma * np.sqrt(T)
-    call_price = S * np.exp(-q * T) * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
-    return call_price
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     price = None
+    delta_value = None
     graphJSON = None
     S = 100
     K = 100
@@ -35,13 +28,16 @@ def index():
             sigma = float(request.form['volatility'])
             q = float(request.form['dividend'])
             
-            price = black_scholes_call(S, K, T, r, sigma, q)
+            price = fl.black_scholes(S, K, T, r, sigma, q)
+            delta_value = fl.delta(S, K, T, r, sigma, q)
+
         except ValueError:
             price = "Erreur dans les entrées. Veuillez entrer des nombres valides."
     
     # Génération du graphique
     S_values = np.linspace(50, 150, 100)
-    prices = [black_scholes_call(s, K, T, r, sigma, q) for s in S_values]
+    prices = [fl.black_scholes(s, K, T, r, sigma, q) for s in S_values]
+    delta_list = [fl.delta(s, K, T, r, sigma, q) for s in S_values]
     
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=S_values, y=prices, mode='lines', name="Prix de l'option Call"))
@@ -51,7 +47,7 @@ def index():
     
     graphJSON = pio.to_json(fig)
     
-    return render_template('index.html', price=price, graphJSON=graphJSON)
+    return render_template('index.html', price=price, delta_value = delta_value, graphJSON=graphJSON)
 
 if __name__ == '__main__':
     app.run(debug=True)
